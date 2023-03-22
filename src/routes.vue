808d0dbb16099ef49b68d9cfe461b931cbb8a16a
Tathagatjha <tathagat1081@gmail.com>
2023-03-22 13:45:34 +0530

    second commit

---
+++ src/routes.vue
@@ -0,0 +1,21 @@
+const routes=[
+{
+  name:"signup"
+  component:"SignUp"
+  path:"/signup"
+}]
+<template>
+    <div class="routes">
+    <button v-on:click="signUp">Login</button>
+   <p>
+     router-link to="/sign-up">Sign Up</router-link>
+   </p>
+</div>
+</template>
+<script>
+
+export default{
+    name:"Login",
+}
+</script>
+
